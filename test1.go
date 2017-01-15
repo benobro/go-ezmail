bd9acfada3abf24e9e977ffb65668b8bd9df2897
root <root@mail.benobro.io>
2017-01-15 16:13:15 -0500

    fixed another typo

--- test1.go
+++ test1.go
@@ -16,7 +16,7 @@ type EmailUser struct {
 
 const emailTemplate = `From someone`
 func main() {
-    eu := &EmailUser{"my@email.com", "PASSWORDr", "smtp.gmail.com", 587}
+    eu := &EmailUser{"my@email.com", "PASSWORD", "smtp.gmail.com", 587}
     auth := smtp.PlainAuth("", eu.Username, eu.Password, eu.EmailServer)
     err := smtp.SendMail(eu.EmailServer+":"+strconv.Itoa(eu.Port), auth, eu.Username, []string{"email@email.com"}, []byte("hello"))
     if err != nil {
