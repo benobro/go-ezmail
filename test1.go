e41cdb9a3be5cd0d71f1fdc7fbc8932e980452be
root <root@mail.benobro.io>
2017-01-14 21:02:30 -0500

    fixed typo

--- test1.go
+++ test1.go
@@ -8,16 +8,17 @@ import (
 )
 
 type EmailUser struct {
-    Username, Password, EmailServer string
-    Port                            int
+    Username, Password, 
+    EmailServer string 
+    Port int
 }
 
-const emailTemplate = `From foo.bar`
 
+const emailTemplate = `From curry`
 func main() {
-    eu := &EmailUser{"benobrien705@gmail.com", "nqjslvzgamidktur", "smtp.gmail.com", 587}
+    eu := &EmailUser{"my@email.com", "PASSWORDr", "smtp.gmail.com", 587}
     auth := smtp.PlainAuth("", eu.Username, eu.Password, eu.EmailServer)
-    err := smtp.SendMail(eu.EmailServer+":"+strconv.Itoa(eu.Port), auth, eu.Username, []string{"lrivera@whitbyschool.org"}, []byte("hello"))
+    err := smtp.SendMail(eu.EmailServer+":"+strconv.Itoa(eu.Port), auth, eu.Username, []string{"email@email.com"}, []byte("hello"))
     if err != nil {
         log.Fatal(err)
     }
