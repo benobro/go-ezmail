0312cfa2919d7182e55fb3e6241ffa7a8d9c5e0d
root <root@mail.benobro.io>
2017-01-16 12:18:04 -0500

    made  and  strings

--- test1.go
+++ test1.go
@@ -13,13 +13,20 @@ type EmailUser struct {
     Port int
 }
 
+type MailContent struct {
+	body string
+	subject string
+}
 
-const emailTemplate = `From someone`
 func main() {
-	msg := "MIME-Version: 1.0" + " \r\n" +
+	
+	mc := &MailContent{"body goes here", "subject goes here"}
+
+	msg := 
+		"MIME-Version: 1.0" + " \r\n" +
 		"Content-type: text/html" + "\r\n" +
-		"Subject: subject goes here" + "\r\n\r\n" +
-		"message goes here" + "\r\n"
+		"Subject: " + mc.subject + "\r\n\r\n" +
+		mc.body + "\r\n"
     eu := &EmailUser{"benobrien705@gmail.com", "nqjslvzgamidktur", "smtp.gmail.com", 587}
     auth := smtp.PlainAuth("", eu.Username, eu.Password, eu.EmailServer)
     err := smtp.SendMail(eu.EmailServer+":"+strconv.Itoa(eu.Port), auth, eu.Username, []string{"ben.obrien000@gmail.com"}, []byte(msg))
