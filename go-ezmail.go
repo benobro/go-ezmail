37aebbd46b57e36ae50c0785429bad4fbe828123
brrm <bruce.mauger@gmail.com>
2017-01-18 20:32:29 +0000

    Turned into package and removed struct

--- go-ezmail.go
+++ go-ezmail.go
@@ -1,35 +1,20 @@
 
-package main
+package ezmail
 
 import (
     "log"
     "net/smtp"
-    "strconv"
 )
 
-type EmailUser struct {
-    Username, Password, 
-    EmailServer string
-    Port int
-}
-
-type MailContent struct {
-	body string
-	subject string
-}
-
-func main() {
-	
-	mc := &MailContent{"body goes here", "subject goes here"}
+func Sendmail(username string, password string, server string, port string, toemail string, subject string, message string) {
 
 	msg := 
 		"MIME-Version: 1.0" + " \r\n" +
 		"Content-type: text/html" + "\r\n" +
-		"Subject: " + mc.subject + "\r\n\r\n" +
-		mc.body + "\r\n"
-    eu := &EmailUser{"send@email.com", "PASSWORD", "smtp.gmail.com", 587}
-    auth := smtp.PlainAuth("", eu.Username, eu.Password, eu.EmailServer)
-    err := smtp.SendMail(eu.EmailServer+":"+strconv.Itoa(eu.Port), auth, eu.Username, []string{"receive@email.com"}, []byte(msg))
+		"Subject: " + subject + "\r\n\r\n" +
+		message + "\r\n"
+    auth := smtp.PlainAuth("", username, password, server)
+    err := smtp.SendMail(server+":"+port, auth, username, []string{toemail}, []byte(msg))
     if err != nil {
         log.Fatal(err)
     }
