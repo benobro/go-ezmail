d250704671b06c6a7dc26906fe394cd43153ee0a
Ben O <ben.obrien000@gmail.com>
2017-02-01 21:31:02 -0500

    added replyto field

--- go-ezmail.go
+++ go-ezmail.go
@@ -12,6 +12,7 @@ func SendMail(username string, password string, server string, port int, toemail
 	msg := 
 		"MIME-Version: 1.0" + " \r\n" +
 		"Content-type: text/html" + "\r\n" +
+		"Reply-To: + replyto + "\r\n" +
 		"Subject: " + subject + "\r\n\r\n" + message + "\r\n"
     auth := smtp.PlainAuth("", username, password, server)
     err := smtp.SendMail(server+":"+strconv.Itoa(port), auth, username, []string{toemail}, []byte(msg))
